58f6f4a536696bb99292fa9ced2f3a85c6dd4814
izofat <gorkemkacar12@gmail.com>
2024-11-26 23:23:17 +0300

    feat: insert queries

---
+++ lyvia_backend/db/query/insert.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from lyvia_backend.db.query.base import BaseQuery
+
+
+class InsertQueries(BaseQuery):
+    def register_account(
+        self, username: str, password: str, name: str, lastName: str, email: str
+    ):
+        query = """
+            INSERT IGNORE INTO user (username, password, name, lastName, email)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+        return self.execute_query(
+            query, username, password, name, lastName, email, is_commit=True
+        )
+
+    def insert_token(self, user_id: int, jwt_token: str, jwt_expire_date: datetime):
+        query = """
+            INSERT INTO token (userId, jwtToken, jwtExpireDate)
+            VALUES (%s, %s, %s)
+        """
+        return self.execute_query(
+            query, user_id, jwt_token, jwt_expire_date, is_commit=True
+        )
--- lyvia_backend/db/query/query.py
+++ lyvia_backend/db/query/query.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from lyvia_backend.db.connection import DbConnection
+from lyvia_backend.db.query.insert import InsertQueries
 from lyvia_backend.db.query.table import TableQueries
 
 
@@ -6,6 +9,14 @@ class Query:
     def __init__(self):
         self.connection = DbConnection()
         self._tables = TableQueries(self.connection)
-
+        self._insert = InsertQueries(self.connection)
         self._tables.create_user_table()
         self._tables.create_token_table()
+
+    def register_account(
+        self, username: str, password: str, name: str, lastName: str, email: str
+    ):
+        return self._insert.register_account(username, password, name, lastName, email)
+
+    def insert_token(self, user_id: int, jwt_token: str, jwt_expire_date: datetime):
+        return self._insert.insert_token(user_id, jwt_token, jwt_expire_date)
