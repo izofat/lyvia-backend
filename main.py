8bb73d54953f3634cfedf10a0ce3bfdd27d8c5f9
izofat <gorkemkacar12@gmail.com>
2024-11-26 22:48:08 +0300

    feat: user blueprint

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
+from lyvia_backend.routes.user import user_bp
 from settings import API_PORT, DEBUG
 
 if __name__ == "__main__":
     app = Flask(__name__)
+    app.register_blueprint(user_bp)
 
     app.run(port=API_PORT, debug=DEBUG)
